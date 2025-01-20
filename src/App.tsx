9cf9ba806a49b054f3789c0b690cccb49c3d3754
PC <PC@DESKTOP-F726TKR>
2025-01-20 21:58:12 +0300

    shadcn all config with code base

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
+    </>
+  )
+}
+
+export default App
